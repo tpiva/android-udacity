e6df6395a5fc8826cb873315ecbe8a65f4d8d399
Thiago <thiago.p.magalhaes@gmail.com>
2017-06-26 21:00:13 -0300

    Mudança layout next e previus steps.

--- AssociateAndroidDeveloperFastTrack/BakingApp/app/src/main/java/com/thiago/bakingapp/activities/RecipeStepsDetailsActivity.java
+++ AssociateAndroidDeveloperFastTrack/BakingApp/app/src/main/java/com/thiago/bakingapp/activities/RecipeStepsDetailsActivity.java
@@ -5,28 +5,37 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.thiago.bakingapp.R;
 import com.thiago.bakingapp.bean.Step;
 import com.thiago.bakingapp.fragments.StepDetailsDescriptionFragment;
-import com.thiago.bakingapp.fragments.StepDetailsNavigateFragment;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static com.thiago.bakingapp.utils.Constants.EXTRA_STEP_SELECTED;
 import static com.thiago.bakingapp.utils.Constants.EXTRA_LIST_STEPS;
 
-public class RecipeStepsDetailsActivity extends AppCompatActivity
-        implements StepDetailsNavigateFragment.OnChangeStepListener{
+public class RecipeStepsDetailsActivity extends AppCompatActivity {
 
     private List<Step> mSteps;
+    private int mCurrentPosition;
     private FragmentManager mFragmentManager;
+
+    @BindView(R.id.step_navigation_previous)
+    Button mBtnPrevious;
+    @BindView(R.id.step_navigation_next)
+    Button mBtnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_steps_details);
-
+        ButterKnife.bind(this);
         Intent intent = getIntent();
 
         if (intent.hasExtra(EXTRA_STEP_SELECTED)
@@ -34,6 +43,7 @@ public class RecipeStepsDetailsActivity extends AppCompatActivity
             mSteps = intent.getParcelableArrayListExtra(EXTRA_LIST_STEPS);
 
             Step step = intent.getParcelableExtra(EXTRA_STEP_SELECTED);
+            mCurrentPosition = step.getId();
 
             StepDetailsDescriptionFragment descriptionFragment = new StepDetailsDescriptionFragment();
             descriptionFragment.setStep(step);
@@ -42,12 +52,31 @@ public class RecipeStepsDetailsActivity extends AppCompatActivity
                     .add(R.id.recipe_step_details_description, descriptionFragment)
                     .commit();
 
-            StepDetailsNavigateFragment navigateFragment = new StepDetailsNavigateFragment();
-            navigateFragment.setCurrentPosition(step.getId());
-            navigateFragment.setMaxSize(mSteps.size());
-            mFragmentManager.beginTransaction()
-                    .add(R.id.recipe_step_details_next, navigateFragment)
-                    .commit();
+            mBtnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mCurrentPosition < mSteps.size()-1) {
+                        mCurrentPosition++;
+                    } else {
+                        // restart
+                        mCurrentPosition = 0;
+                    }
+                    nextPreviousStep();
+                }
+            });
+
+            mBtnPrevious.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mCurrentPosition > 0) {
+                        mCurrentPosition--;
+                    } else {
+                        // go back to end, cycle
+                        mCurrentPosition = (mSteps.size() - 1);
+                    }
+                    nextPreviousStep();
+                }
+            });
         }
     }
     @Override
@@ -60,10 +89,9 @@ public class RecipeStepsDetailsActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void newPosition(int position) {
+    private void nextPreviousStep() {
         StepDetailsDescriptionFragment descriptionFragment = new StepDetailsDescriptionFragment();
-        descriptionFragment.setStep(mSteps.get(position));
+        descriptionFragment.setStep(mSteps.get(mCurrentPosition));
 
         mFragmentManager.beginTransaction()
                 .replace(R.id.recipe_step_details_description, descriptionFragment)
