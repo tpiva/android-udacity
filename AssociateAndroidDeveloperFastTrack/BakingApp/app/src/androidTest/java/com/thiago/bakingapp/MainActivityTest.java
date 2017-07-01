537a2abfecab629147e2cd88fd79e9a42223621c
Thiago <thiago.p.magalhaes@gmail.com>
2017-07-01 19:02:07 -0300

    Mais testes.

--- AssociateAndroidDeveloperFastTrack/BakingApp/app/src/androidTest/java/com/thiago/bakingapp/MainActivityTest.java
+++ AssociateAndroidDeveloperFastTrack/BakingApp/app/src/androidTest/java/com/thiago/bakingapp/MainActivityTest.java
@@ -1,26 +1,41 @@
 package com.thiago.bakingapp;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.thiago.bakingapp.activities.MainActivity;
 import com.thiago.bakingapp.adapter.RecipeAdapter;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.hasLinks;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -70,6 +85,42 @@ public class MainActivityTest {
         onView(withId(R.id.recipe_details_steps)).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void onClickStep() {
+        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.recipe_details_steps)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.step_description)).check(matches(withText("Recipe Introduction")));
+        onView(allOf(withId(R.id.step_description_details),
+                hasDescendant(withId(R.id.step_exo_video)))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.step_description_details),hasLinks()));
+
+    }
+
+    @Test
+    public void onClickNextStep() {
+        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.recipe_details_steps)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.step_navigation_next)).perform(click());
+        onView(withId(R.id.step_description))
+                .check(matches(withText(startsWith("1. Preheat the oven to"))));
+        onView(allOf(withId(R.id.step_description_details),
+                hasDescendant(withId(R.id.step_image)))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.step_description_details),not(hasLinks())));
+        onView(withId(R.id.step_image)).check(matches(withImage(R.drawable.default_food)));
+    }
+
+    @Test
+    public void onClickPreviousStep() {
+        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.recipe_details_steps)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.step_navigation_previous)).perform(click());
+        onView(withId(R.id.step_description))
+                .check(matches(withText(startsWith("9. Cut and serve."))));
+        onView(allOf(withId(R.id.step_description_details),
+                hasDescendant(withId(R.id.step_exo_video)))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.step_description_details),hasLinks()));
+    }
+
     private static Matcher<RecyclerView.ViewHolder> withContent(final String textToDisplay) {
         return new BoundedMatcher<RecyclerView.ViewHolder, RecipeAdapter.RecipeViewHolder>
                 (RecipeAdapter.RecipeViewHolder.class) {
@@ -82,6 +133,36 @@ public class MainActivityTest {
             @Override
             public void describeTo(Description description) {
                 description.appendText("view holder with title: " + textToDisplay);
+            }
+        };
+    }
+
+    private static TypeSafeMatcher<View> withImage(final int expectedId) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("comparingImages");
+            }
+
+            @Override
+            protected boolean matchesSafely(View target) {
+                if (!(target instanceof ImageView)){
+                    return false;
+                }
+                ImageView imageView = (ImageView) target;
+                if (expectedId < 0){
+                    return imageView.getDrawable() == null;
+                }
+                Resources resources = target.getContext().getResources();
+                Drawable expectedDrawable = resources.getDrawable(expectedId);
+                if (expectedDrawable == null) {
+                    return false;
+                }
+                BitmapDrawable bmd = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = bmd.getBitmap();
+                BitmapDrawable expected = (BitmapDrawable) expectedDrawable;
+                Bitmap otherBitmap = expected.getBitmap();
+                return bitmap.sameAs(otherBitmap);
             }
         };
     }
--- AssociateAndroidDeveloperFastTrack/BakingApp/app/src/androidTest/java/com/thiago/bakingapp/RecipeDetailsStepTest.java
+++ AssociateAndroidDeveloperFastTrack/BakingApp/app/src/androidTest/java/com/thiago/bakingapp/RecipeDetailsStepTest.java
@@ -12,12 +12,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.core.IsNot.not;
 
 
 @RunWith(AndroidJUnit4.class)
