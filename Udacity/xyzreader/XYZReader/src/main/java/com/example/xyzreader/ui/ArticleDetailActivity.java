8357fa17a746cd1c261dce8bee8da1a883f8177f
Thiago <thiago.p.magalhaes@gmail.com>
2017-11-16 21:32:08 -0200

    View pager animation.

--- Udacity/xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ Udacity/xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +25,7 @@ import com.example.xyzreader.data.ItemsContract;
 /**
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
-public class ArticleDetailActivity extends ActionBarActivity
+public class ArticleDetailActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Cursor mCursor;
@@ -56,6 +58,7 @@ public class ArticleDetailActivity extends ActionBarActivity
         mPager.setPageMargin((int) TypedValue
                 .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
         mPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
 
         mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
@@ -74,6 +77,8 @@ public class ArticleDetailActivity extends ActionBarActivity
                 mSelectedItemId = mCursor.getLong(ArticleLoader.Query._ID);
                 updateUpButtonPosition();
             }
+
+
         });
 
         mUpButtonContainer = findViewById(R.id.up_container);
